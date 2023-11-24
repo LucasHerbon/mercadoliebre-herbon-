1be1e847bc74152274db0bd57e24fca6e669df74
fraroxx <luherbon@gmail.com>
2023-11-23 21:41:09 -0300

    Arreglando el npm start

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-
-app.listen(3030, () => console.log ("Servidor corriendo en: http://localhost:3030/"));
-
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log ("Servidor corriendo en: http://localhost:3030/"));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
 });
