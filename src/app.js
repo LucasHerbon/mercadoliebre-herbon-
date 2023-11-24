63a2028394c4af280d33131a2f594e20eb78d90f
fraroxx <luherbon@gmail.com>
2023-11-23 22:27:23 -0300

    Moviendo el app.js a la carpeta src

--- src/app.js
+++ src/app.js
@@ -3,29 +3,29 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log ("Servidor corriendo en: http://localhost:3030/"));
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/register.html"))
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/login.html"))
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
 });
 
 //esto es solo para cumplir con lo visual del mercado liebre
 app.post("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
 app.post("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
